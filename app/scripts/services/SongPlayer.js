c129b3451f1fcd6c925ecb9fd115de715dce0482
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2016-12-07 15:44:36 -0600

    Assignment 7 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,10 @@
 (function() {
      function SongPlayer() {
         var SongPlayer = {};
-         
+        /**
+        * @desc currently playing song object
+        * @type {Object}
+        */
         var currentSong = null;
         /**
         * @desc Buzz object audio file
@@ -27,19 +30,39 @@
  
             currentSong = song;
         };
-         
-         SongPlayer.play = function(song) {
+        
+        /**
+        * @function playSong
+        * @desc Plays the currentBuzzObject
+        * @param {Object} song
+        */ 
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;  
+        };
+        
+         /**
+        * @function SongPlayer.play
+        * @desc public method that compares currentSong to song and enables user to play song accordingly
+        * @param {Object} song
+        */ 
+        SongPlayer.play = function(song) {
              if (currentSong !== song) {
                 setSong(song);     
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                 currentBuzzObject.play();
                 }
             }    
      };
-         
+        
+         /**
+        * @function SongPlayer.pause
+        * @desc public method that pauses the currentBuzzObject
+        * @param {Object} song
+        * @returns {Object}
+        */ 
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
