3eabbcea4eca2fe412045c8d3ddc01e809665e28
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2016-12-08 10:27:24 -0600

    Checkpoint 8 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,11 +1,11 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
         var SongPlayer = {};
-        /**
-        * @desc currently playing song object
+         /**
+        * @desc stores the current album data by using the Fixtures service to retrieve album info
         * @type {Object}
         */
-        var currentSong = null;
+        var currentAlbum = Fixtures.getAlbum();
         /**
         * @desc Buzz object audio file
         * @type {Object}
@@ -20,7 +20,7 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
         }
  
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -28,7 +28,7 @@
                 preload: true
             });
  
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
         
         /**
@@ -40,17 +40,34 @@
             currentBuzzObject.play();
             song.playing = true;  
         };
-        
+         
+         /**
+        * @function getSongIndex
+        * @desc gets the index of a song
+        * @param {Object} song
+        * @returns {Number}
+        */ 
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
+         
+        /**
+        * @desc active song object from list of songs
+        * @type {Object}
+        */
+       SongPlayer.currentSong = null;
+         
          /**
         * @function SongPlayer.play
-        * @desc public method that compares currentSong to song and enables user to play song accordingly
+        * @desc public method that compares SongPlayer.currentSong to song and enables user to play song accordingly
         * @param {Object} song
         */ 
         SongPlayer.play = function(song) {
-             if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+             if (SongPlayer.currentSong !== song) {
                 setSong(song);     
                 playSong(song);
-            } else if (currentSong === song) {
+            } else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                 currentBuzzObject.play();
                 }
@@ -64,8 +81,27 @@
         * @returns {Object}
         */ 
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
+        };
+        
+         /**
+        * @function SongPlayer.previous
+        * @desc public method that moves to the previous song
+        */ 
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+            
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
         };
          
           return SongPlayer;
